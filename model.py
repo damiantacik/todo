dd0a9f143dce8c67f49b56c6b56c8062cc4f14eb
damtac <damiantacik@gmail.com>
2021-06-10 20:55:33 +0200

    add toggle task status and delete task

--- model.py
+++ model.py
@@ -33,9 +33,30 @@ def add_todo(tasks, title, duration=60):
     tasks.append(todo)
 
 
-add_todo(todos, "cut")
-add_todo(todos, "abc")
-add_todo(todos, "cde", 120)
-pp(todos)
+def find_todo_by_id(tasks, idx):
+    for task in tasks:
+        if task["id"] == idx:
+            return task
 
+
+def toggle_status(tasks, idx):
+    task = find_todo_by_id(tasks, idx)
+    task["status"] = not task["status"]
+
+
+def delete_todo(tasks, idx):
+    task = find_todo_by_id(tasks, idx)
+    tasks.remove(task)  # works in lists, remove one task
+
+
+# add_todo(todos, "cut")
+# add_todo(todos, "abc")
+# add_todo(todos, "cde", 120)
+#
+# toggle_status(todos, 5)
+# toggle_status(todos, 5)
+delete_todo(todos, 5)
+pp(todos)
+#
+# pp(find_todo_by_id(todos, 6))
 
