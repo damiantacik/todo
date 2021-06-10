b3ac29d7b1a439114d1a006d036303d641b55049
damtac <damiantacik@gmail.com>
2021-06-10 21:57:37 +0200

    add status and sorting by date

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from pprint import pprint as pp
 from helpers import generate_id
 
@@ -10,14 +10,14 @@ todos = [
         "title": "learn python",
         "status": False,
         "duration": 10,
-        "created_date": datetime.now()
+        "created_date": datetime.now() - timedelta(days=3)
     },
     {
         "id": 5,
         "title": "learn python",
         "status": False,
         "duration": 10,
-        "created_date": datetime.now()
+        "created_date": datetime.now() - timedelta(days=6)
     }
 ]
 
@@ -49,14 +49,28 @@ def delete_todo(tasks, idx):
     tasks.remove(task)  # works in lists, remove one task
 
 
-# add_todo(todos, "cut")
-# add_todo(todos, "abc")
-# add_todo(todos, "cde", 120)
-#
+def get_todos_with_status(tasks, status):
+    tasks_with_status = []
+
+    for task in tasks:
+        if task["status"] == status:
+            tasks_with_status.append(task)
+
+    return tasks_with_status
+
+
+def get_todos_by_date(tasks, order="asc"):
+    order_bool = True if order == "des" else False
+    return sorted(tasks, key=lambda task: task["created_date"], reverse=order_bool)
+
+
+add_todo(todos, "cut")
+add_todo(todos, "abc")
+add_todo(todos, "cde", 120)
 # toggle_status(todos, 5)
-# toggle_status(todos, 5)
-delete_todo(todos, 5)
-pp(todos)
-#
+# toggle_status(todos, 6)
+# delete_todo(todos, 5)
+# pp(get_todos_with_status(todos, True))
 # pp(find_todo_by_id(todos, 6))
+pp(get_todos_by_date(todos, "des"))
 
