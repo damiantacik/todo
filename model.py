4b54eabb5d683e889b6ff1470ce60c0397d8c872
damtac <damiantacik@gmail.com>
2021-06-10 17:04:28 +0000

    create todos model and samples data

---
+++ model.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+todos = [
+    {
+        "id": 1,
+        "title": "learn python",
+        "status": False,
+        "duration": 10,
+        "created_date": datetime.now()
+    }
+]
